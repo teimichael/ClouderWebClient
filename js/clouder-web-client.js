3119d9d1794029622f4d5c91e8d2f3c6cdb4581b
HKZou <550402753@qq.com>
2020-04-08 22:48:44 +0800

    add folder API

--- js/clouder-web-client.js
+++ js/clouder-web-client.js
@@ -1,5 +1,5 @@
 ;(function () {
-    "use strict";
+    'use strict';
     let _global;
 
     const CODE = {
@@ -7,11 +7,19 @@
         failure: -1
     };
 
-    const SERVER = 'http://35.190.234.235:9110';
+    /* const SERVER = 'http://35.190.234.235:9110';*/
+    const SERVER = 'http://104.198.116.231:9110';
 
     const URL = {
-        login: SERVER + '/access/third/login',
-        logout: SERVER + '/access/logout'
+        /*login: SERVER + '/access/third/login',*/
+        login: SERVER + '/access/login',
+        logout: SERVER + '/access/logout',
+        register: SERVER + '/access/register',
+        folder: SERVER + '/folder/',
+        item: SERVER + '/item/',
+        search: SERVER + '/search/',
+        secure: SERVER + '/secure/',
+        user: SERVER + '/user/',
     };
 
     let globalData = {
@@ -28,33 +36,45 @@
             this.password = password;
             this.source = source;
         },
-        login: function (authRegister, callbacks) {
+        RegisterVO: function (avatar, info, name, password, source, thirdId, username, uuid) {
+            this.avatar = avatar;
+            this.info = info;
+            this.name = name;
+            this.password = password;
+            this.source = source;
+            this.thirdId = thirdId;
+            this.username = username;
+            this.uuid = uuid;
+        },
+        login: function (authLogin, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
             NA_ajax({
-                type: "POST",
+                type: 'POST',
                 url: URL.login,
-                dataType: "json",
-                data: JSON.stringify(authRegister),
+                dataType: 'json',
+                data: JSON.stringify(authLogin),
                 contentType: 'application/json',
                 success: function (data) {
                     if (data.code === CODE.success) {
                         globalData.token = data.data;
-                        callbacks.onSuccess(data)
+                        onSuccess(data)
                     } else {
-                        callbacks.onFailure(data)
+                        onFailure(data)
                     }
                 },
                 error: function () {
-                    console.log("login error")
+                    console.log('login error')
                 }
             })
         },
         logout: function () {
             NA_ajax({
-                type: "POST",
+                type: 'POST',
                 url: URL.logout,
-                dataType: "json",
+                dataType: 'json',
                 beforeSend: function (xhr) {
-                    xhr.setRequestHeader("Authorization", globalData.token);
+                    xhr.setRequestHeader('Authorization', globalData.token);
                 },
                 success: function (data) {
                     if (data.code === CODE.success) {
@@ -64,20 +84,309 @@
                     }
                 },
                 error: function () {
-                    console.log("Logout error")
+                    console.log('Logout error')
                 }
             })
-        }
+        },
+        register: function (RegisterVO, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'POST',
+                url: URL.register,
+                dataType: 'json',
+                data: JSON.stringify(RegisterVO),
+                contentType: 'application/json',
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('Register successfully.');
+                        onSuccess(data)
+                    } else {
+                        alert(data.message);
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('Register error')
+                }
+            })
+        },
+        getFolder: function (folderId, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'GET',
+                url: URL.folder + folderId,
+                dataType: 'json',
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('Get folder successfully.');
+                        onSuccess(data)
+                    } else {
+                        alert(data.message);
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('Get folder error')
+                }
+            })
+        },
+        getFolderContent: function ({folderId, pageParams}, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            let pageInfo = {
+                page: pageParams.page,
+                size: pageParams.size
+            }
+            NA_ajax({
+                type: 'GET',
+                url: URL.folder + folderId + '/content',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: pageInfo,
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('Get folder successfully.');
+                        onSuccess(data)
+                    } else {
+                        alert(data.message);
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('Get folder error')
+                }
+            })
+        },
+        getFolderContentByPath: function ({folderPath, pageParams}, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            let folderInfo = {
+                folderPath: folderPath,
+                page: pageParams.page,
+                size: pageParams.size
+            }
+            console.log(JSON.stringify(folderInfo))
+            NA_ajax({
+                type: 'GET',
+                url: URL.folder + 'content/by/path',
+                dataType: 'json',
+                /*data: 'folderPath=root%2F&page=0&size=20',*/
+                data: folderInfo,
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('Get folder successfully.');
+                        onSuccess(data)
+                    } else {
+                        alert(data.message);
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('Get folder error')
+                }
+            })
+        },
+        copyFolder: function ({folderId, destFolderId}, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'POST',
+                url: URL.folder + 'copy/' + folderId + '/to/' + destFolderId,
+                dataType: 'json',
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('copy successful')
+                        onSuccess(data)
+                    } else {
+                        console.log('copy failed')
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('copy error')
+                }
+            })
+        },
+        createFolder: function ({folderName, parentFolderId}, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'POST',
+                url: URL.folder + 'create/in/' + parentFolderId,
+                dataType: 'json',
+                query: {folderName: folderName},
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('create folder successful')
+                        onSuccess(data)
+                    } else {
+                        console.log('create folder failed')
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('create folder error')
+                }
+            })
+        },
+        deleteFolder: function (folderId, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'POST',
+                url: URL.folder + 'delete/' + folderId,
+                dataType: 'json',
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('delete folder successful')
+                        onSuccess(data)
+                    } else {
+                        console.log('delete folder failed')
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('delete folder error')
+                }
+            })
+        },
+        getFolderList: function (parentFolderId, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'GET',
+                url: URL.folder + 'list/' + parentFolderId,
+                dataType: 'json',
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('Get folder list successfully.');
+                        onSuccess(data)
+                    } else {
+                        alert(data.message);
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('Get folder list error')
+                }
+            })
+        },
+        moveFolder: function ({folderId, destFolderId}, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'POST',
+                url: URL.folder + 'move/' + folderId + '/to/' + destFolderId,
+                dataType: 'json',
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('copy successful')
+                        onSuccess(data)
+                    } else {
+                        console.log('copy failed')
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('copy error')
+                }
+            })
+        },
+        renameFolder: function ({folderId, name}, callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'POST',
+                url: URL.folder + 'rename/' + folderId,
+                dataType: 'json',
+                query: {name: name},
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('rename folder successful')
+                        onSuccess(data)
+                    } else {
+                        console.log('rename folder failed')
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('rename folder error')
+                }
+            })
+        },
+        getInfo: function (callbacks) {
+            let onSuccess = callbacks.onSuccess || function () {}
+            let onFailure = callbacks.onFailure || function () {}
+            NA_ajax({
+                type: 'GET',
+                url: URL.user + 'get/info',
+                dataType: 'json',
+                contentType: 'application/json',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', globalData.token);
+                },
+                success: function (data) {
+                    if (data.code === CODE.success) {
+                        console.log('Get info successfully.');
+                        onSuccess(data)
+                    } else {
+                        alert(data.message);
+                        onFailure(data)
+                    }
+                },
+                error: function () {
+                    console.log('Get info error')
+                }
+            })
+        },
     };
 
     function NA_ajax() {
         let ajaxData = {
-            type: arguments[0].type || "GET",
-            url: arguments[0].url || "",
-            async: arguments[0].async || "true",
+            type: arguments[0].type || 'GET',
+            url: arguments[0].url || '',
+            async: arguments[0].async || 'true',
             data: arguments[0].data || null,
-            dataType: arguments[0].dataType || "text",
-            contentType: arguments[0].contentType || "application/x-www-form-urlencoded",
+            query: arguments[0].query || null,
+            dataType: arguments[0].dataType || 'text',
+            contentType: arguments[0].contentType || 'application/x-www-form-urlencoded',
             beforeSend: arguments[0].beforeSend || function () {
             },
             success: arguments[0].success || function () {
@@ -87,9 +396,14 @@
         };
         let xhr = createxmlHttpRequest();
         xhr.responseType = ajaxData.dataType;
+        if(ajaxData.type === 'GET') {
+            ajaxData.url = buildURL(ajaxData.url, ajaxData.data)
+        } else if(ajaxData.query !== undefined) {
+            ajaxData.url = buildURL(ajaxData.url, ajaxData.query)
+        }
         xhr.open(ajaxData.type, ajaxData.url, ajaxData.async);
         ajaxData.beforeSend(xhr)
-        xhr.setRequestHeader("Content-Type", ajaxData.contentType);
+        xhr.setRequestHeader('Content-Type', ajaxData.contentType);
         xhr.send(convertData(ajaxData.data));
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
@@ -104,7 +418,7 @@
 
     function createxmlHttpRequest() {
         if (window.ActiveXObject) {
-            return new ActiveXObject("Microsoft.XMLHTTP");
+            return new ActiveXObject('Microsoft.XMLHTTP');
         } else if (window.XMLHttpRequest) {
             return new XMLHttpRequest();
         }
@@ -114,7 +428,7 @@
         if (typeof data === 'object') {
             let convertResult = "";
             for (let c in data) {
-                convertResult += c + "=" + data[c] + "&";
+                convertResult += c + '=' + data[c] + '&';
             }
             convertResult = convertResult.substring(0, convertResult.length - 1)
             return convertResult;
@@ -123,12 +437,35 @@
         }
     }
 
+    function buildURL(url, params) {
+        if (!params) {
+            return url;
+        }
+        const parts = []
+        for(let key in params) {
+            parts.push(encode(key) + '=' + encode(params[key]))
+        }
+        console.log(url + '?' + parts.join('&'))
+        return url + '?' + parts.join('&')
+    }
+
+    function encode(val) {
+        return encodeURIComponent(val).
+        replace(/%40/gi, '@').
+        replace(/%3A/gi, ':').
+        replace(/%24/g, '$').
+        replace(/%2C/gi, ',').
+        replace(/%20/g, '+').
+        replace(/%5B/gi, '[').
+        replace(/%5D/gi, ']');
+    }
+
     _global = (function () {
         return this || (0, eval)('this');
     }());
-    if (typeof module !== "undefined" && module.exports) {
+    if (typeof module !== 'undefined' && module.exports) {
         module.exports = ClouderWebClient;
-    } else if (typeof define === "function" && define.amd) {
+    } else if (typeof define === 'function' && define.amd) {
         define(function () {
             return ClouderWebClient;
         });
--- js/test.js
+++ js/test.js
@@ -3,6 +3,17 @@ function login() {
     let onSuccess = function (data) {
         alert(data.message);
         console.log(data);
+        const pageParams = {
+            page: 0,
+            size: 20
+        }
+        let fnCallbacksOnSuccess = function (data) {
+            console.log(data);
+        }
+        let fnCallbacks = new ClouderWebClient.Callbacks(fnCallbacksOnSuccess);
+        ClouderWebClient.getInfo(fnCallbacks)
+        /* ClouderWebClient.createFolder({folderName:'Folder1', parentFolderId:'2'}, fnCallbacks)*/
+        ClouderWebClient.getFolderList(2, fnCallbacks)
     };
     let onFailure = function (data) {
         alert(data.message);
